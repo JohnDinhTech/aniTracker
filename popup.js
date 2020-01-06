fe34d5e13f9d226f5aa4de8649446c2fb1f7fb0b
John Dinh <johnduydinh@gmail.com>
2020-01-06 09:34:59 -0500

    only allow user to toggle switch if website is compatible

--- popup.js
+++ popup.js
@@ -1,18 +1,19 @@
 const switchColor = document.querySelector(".switch-selection");
 const offSwitch = document.getElementById("off");
 const onSwitch = document.getElementById("on");
+const noTrack = document.querySelector(".no-track");
 
 offSwitch.addEventListener("click", () => {
-    switchColor.style.backgroundColor = "red";
-    console.log(switchColor.style.backgroundColor);
+	switchColor.style.backgroundColor = "#F08080";
+	console.log(switchColor.style.backgroundColor);
 });
 
 onSwitch.addEventListener("click", () => {
-    switchColor.style.backgroundColor = "#65bd63";
+	switchColor.style.backgroundColor = "#65bd63";
 });
 
-chrome.tabs.getSelected(null, tab => {
-    if (tab.url.includes("episode")) {
-        // alert("YAY");
-    }
+chrome.tabs.getSelected(null, (tab) => {
+	if (tab.url.toLowerCase().includes("episode")) {
+		noTrack.classList.add("no-track-off");
+	}
 });
