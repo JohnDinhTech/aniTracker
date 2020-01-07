7ea303fa39d9789452b971b48df5a06f56bb83cc
John Dinh <johnduydinh@gmail.com>
2020-01-07 08:51:13 -0500

    able to find title from gogoanime

--- background.js
+++ background.js
@@ -9,6 +9,30 @@ chrome.runtime.onInstalled.addListener(() => {
 	});
 });
 
+chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
+	chrome.tabs.getSelected(null, (tab) => {
+		const isCompatible = tab.url.toLowerCase().includes("episode");
+		if (isCompatible && changeInfo.status === "complete") {
+			const url = tab.url.toLowerCase();
+			const episodeNumberUrl = url.match(/episode\D+\d+/g);
+			const episodeNumber = parseInt(episodeNumberUrl[0].match(/\d+/g));
+			const baseUrlLength = url.match(/\w+:\/\/[\w+.]+\//g)[0].length;
+			let titleName = url.slice(
+				baseUrlLength,
+				url.indexOf(episodeNumberUrl) - 1
+			);
+			titleName = titleName.replace(/-/g, " ");
+			titleName = titleName
+				.split(" ")
+				.map((s) => s.charAt(0).toUpperCase() + s.substring(1))
+				.join(" ");
+
+			alert(titleName);
+			// alert(url.indexOf(episodeNumberUrl));
+			// alert(`You are on episode ${episodeNumber}`);
+		}
+	});
+});
 // this is code that runs everytime a page is updated
 // chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
 //     if (changeInfo.status === "complete") {
