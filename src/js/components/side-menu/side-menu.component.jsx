bcd3043de9f2a6282a2d1966d5e1196e6251d800
John Dinh <johnduydinh@gmail.com>
2020-01-15 09:03:30 -0500

    remove unneeded className from side-menu's img

--- src/js/components/side-menu/side-menu.component.jsx
+++ src/js/components/side-menu/side-menu.component.jsx
@@ -2,11 +2,11 @@ import React from "react";
 import "./side-menu.styles.css";
 import logo from "../../../img/aniTracker32.png";
 
-export const SideMenu = (props) => (
-	<div className='side-menu'>
-		<h1>
-			<img class='logo' src={logo} alt='aniTrack Logo' />
-			aniTracker
-		</h1>
-	</div>
+export const SideMenu = props => (
+    <div className="side-menu">
+        <h1>
+            <img src={logo} alt="aniTrack Logo" />
+            aniTracker
+        </h1>
+    </div>
 );
